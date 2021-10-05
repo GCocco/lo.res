4eff03f771e778b7b8a81d3e013e82d071b29ef4
GCocco <giammarco.cocco@gmail.com>
2021-10-05 17:09:11 +0200

    added pipes.py file for prefab pipes

---
+++ pipes.py
@@ -0,0 +1,22 @@
+import dialogpipe, pipe
+import pipelements, structures
+
+
+class MainMenu(dialogpipe.DialogPipe):
+    def __init__(self):
+        super().__init__("Main Menu")
+        self._button_start = dialogpipe.DialogButton(self, "Start Game", command=WorldMap().stack_push)
+
+        self._button_exit = dialogpipe.DialogButton(self, "Exit", command=self.stack_pop)
+        pass
+    pass
+
+
+class WorldMap(pipe.MapPipe):
+    def __init__(self):
+        super().__init__()
+        structures.PipeTreeBox(self, (3,3))
+        self._avatar = pipelements.Avatar(self, (0,0))
+        pass
+    pass
+
